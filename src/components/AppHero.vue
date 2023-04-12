1ce6f91589b9662db9d0c067de5e626d01aebb19
FilippoFoti <filippofoti7@gmail.com>
2023-04-12 13:53:01 +0200

    Completed Exercise + bonus

---
+++ src/components/AppHero.vue
@@ -0,0 +1,26 @@
+<script>
+
+export default {
+    name: "AppHero",
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<template>
+    <img src="../assets/img/pexels-photo-248360.jpeg" alt="Application">
+</template>
+
+<style>
+img {
+    width: 100%;
+    height: 200px;
+    object-fit: cover;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,32 @@
+<script>
+
+export default {
+    name: "AppTitle",
+    data() {
+        return {
+            title: "Vite",
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<template>
+    <h1>La mia prima app con <span>&nbsp;{{ title }}</span>!</h1>
+</template>
+
+<style>
+h1 {
+    display: flex;
+    justify-content: center;
+    font-size: 4rem;
+    color: rgba(0, 22, 50, 1);;
+}
+
+span {
+    color: rgba(0, 223, 107, 1);
+}
+
+</style>
